7faa4bc7d48b94ecea86505e5a2ef0cd0dbe4999
Eric Liao <ericliao57@gmail.com>
2023-01-11 00:05:06 +1000

    more d3.js stuff

--- personal/graph.js
+++ personal/graph.js
@@ -1,10 +1,11 @@
 data = ['a', 'b', 'c', 'd', 'e', 'f']
 
-
-
 d3.select(".graph-container")
-    .append('circle')
-    .attr('class', 'graph-node')
-    .attr('cx', '50%')
-    .attr('cy', '50%')
-    .attr('r', 20);
+    .selectAll('div')   // 0 div's in graph-conatiner
+    .data(data)     // 6 divs in data
+    .enter()        // an enter node is creating for the missing data 6 - 0 = 6
+    .append('div')  // appends 6 of the missing data
+    .attr('class', 'graph-node')    // gives an attribute 
+    .text(d => d);  // displays the text -> can also display the index with (d, i) => i
+
+
