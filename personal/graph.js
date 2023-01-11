10b90468486974e1b389ca2cc5cd59dd8cc29e17
Eric Liao <ericliao57@gmail.com>
2023-01-11 18:19:45 +1000

    mouse hover over nodes calls stroke draw function

--- personal/graph.js
+++ personal/graph.js
@@ -23,35 +23,55 @@ graph = {
         { "source": 'Home', "target": 'LinkedIn' },
         { "source": 'Personal', "target": 'Youtube' },
         { "source": 'Personal', "target": 'Twitch' },
+        { "source": 'Personal', "target": 'Instagram' },
+        { "source": 'Twitter', "target": 'MyAnimeList' },
+        { "source": 'Tetrio', "target": 'Jstris' },
     ]
 };
 
 // getting the graph container
-var conatiner = d3.select('.graph-container')
-var width = conatiner.attr("width");
-var height = conatiner.attr("height");
+let container = d3.select('.graph-container');
+var width = 1000;
+var height = 500;
+var radius = 10;
 
 // Force simulation
 const simulation = d3.forceSimulation(graph.nodes)
-    .force("charge", d3.forceManyBody().strength(-30))
-    .force("link", d3.forceLink(graph.links).id(function id(d) {return d.id;}).links(graph.links))
-    .force("center", d3.forceCenter(500, 250))
+    .force("gravity", d3.forceManyBody().strength(1))
+    .force("charge", d3.forceManyBody().strength(-50))
+    .force("link", d3.forceLink(graph.links).id(function id(d) {return d.id;}).distance(70))
+    .force("center", d3.forceCenter(width / 2, height / 2))
     .on("tick", ticked);
 
 // Binding the force simulation to the data of links
-var link = conatiner.append("g")    // this "g" is a container for the svg
+var link = container.append("g")    // this "g" is a container for the svg
     .selectAll('line')
     .data(graph.links)
     .join('line')
     .attr('class', 'graph-link');
 
-// Bind the force simulation to the data of nodes
-var node = conatiner.append("g")
+var nodeTextShow = function(d) {
+    d3.select(this)
+        .style("stroke", "black")
+}
+
+var nodeTexthide = function(d) {
+    d3.select(this)
+      .style("stroke", "none")
+}
+
+// Binding the force simulation to the data of nodes
+var node = container.append("g")
     .selectAll('circle')
     .data(graph.nodes)
     .join('circle')
     .attr('class', 'graph-node')
+    .attr('r', radius)
+    .on("mouseover", nodeTextShow)
+    .on("mouseout", nodeTexthide)
     .call(drag(simulation));
+
+    
 
 function ticked() {
 link
@@ -60,11 +80,16 @@ link
     .attr("x2", d => d.target.x)
     .attr("y2", d => d.target.y);
 
-node
-    .attr("cx", d => d.x)
-    .attr("cy", d => d.y);
+// node
+//     .attr("cx", d => d.x)
+//     .attr("cy", d => d.y);
+
+// Function so that the nodes do not go out of the graph-container
+node.attr("cx", function(d) { return d.x = Math.max(radius, Math.min(width - radius, d.x)); })
+    .attr("cy", function(d) { return d.y = Math.max(radius, Math.min(height - radius, d.y)); });
 }
 
+// function for dragging nodes around
 function drag(simulation) {    
     function dragstarted(event) {
         if (!event.active) simulation.alphaTarget(0.2).restart();
@@ -88,15 +113,4 @@ function drag(simulation) {
         .on("drag", dragged)
         .on("end", dragended);
 }
-// d3.select(".graph-container")
-//     .selectAll('circle')   // 0 div's in graph-conatiner
-//     .data(graph.nodes)     // 6 divs in data
-//     .enter()        // an enter node is creating for the missing data 6 - 0 = 6
-//     .append('circle')  // appends 6 of the missing data
-//     .attr('class', 'graph-node')    // gives an attribute 
-//     .attr('r', 20)
-//     .attr('cx', '50%')
-//     .attr('cy', '50%')
-//     .text(d => d.id);  // displays the text -> can also display the index with (d, i) => i
-
 
